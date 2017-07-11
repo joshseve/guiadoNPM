d4be5dffd5b8f0f99f4bbe74b6cd62a25e2060cc
joshseve <josh.seve@gmail.com>
2017-07-11 16:37:18 -0500

    actualizaciones del materialize

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ app.get('/', (req, res)=>{
   res.sendFile(__dirname+'/index.html');
 });
 app.listen(8080);
+
+app.use('/static', express.static(path.join(__dirname, 'node_modules')));
+app.use('/static', express.static(path.join(__dirname, 'assets')));
